ad6e59062ef93a888781a2912e2d17e6b9552a6c
Philip Ho <pho86@se14l0124-04.ad.bcit.ca>
2022-02-17 16:40:28 -0800

    week 7

---
+++ exs/ex6/comps/gamemenu.js
@@ -0,0 +1,48 @@
+//MUST HAVE - CREATE A TEMPLATE TAG
+var template_menu = document.createElement("template"); //<template> </template> RULE
+
+//To-do - CREATE THE UI HERE!
+template_menu.innerHTML = `
+<style> 
+.menu {
+    color:black;
+    background-color: white;
+    width: 5%;
+}
+</style>
+<div class='menu'>
+<p>snow</p>
+</div>
+`;
+
+//MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
+class GameMenu extends HTMLElement {
+
+    //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
+    constructor(){
+        super(); //pass on the HTMLElement super powers!
+        this.attachShadow({mode:"open"}) //Attach it to the shadowRoot
+
+        //To-do - CREATE THE STATES FOR THE UI HERE!
+    }
+
+    //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
+    connectedCallback(){
+        this.shadowRoot.appendChild(template_menu.content.cloneNode(true)); //use the template to make a clone
+        if (this.getAttribute("name")) {
+            this.shadowRoot.querySelector(".menu").innerText = this.getAttribute("name");
+        }
+        this.shadowRoot.querySelector(".menu").onclick = () => 
+        {
+            document.querySelector('#gamebg').changeBG(`./imgs/${this.getAttribute('name')}.svg`);
+            document.querySelector('#gamepar').changeParticles(this.getAttribute('name'));
+        }
+
+        //change the text within innerHTML //all snow becomes all forest
+    }
+
+    //To-do - CREATE THE FUNCTIONALITIES HERE!
+}
+
+//MUST HAVE - define the tag for the custom elements
+customElements.define("game-menu", GameMenu)
